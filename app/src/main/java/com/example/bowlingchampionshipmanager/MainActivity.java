3762dc3ea9c5338a929b6ece53494b252ab80a5c
anaspits <minianast@yahoo.gr>
2019-11-17 19:38:31 +0200

    fist activity

--- app/src/main/java/com/example/bowlingchampionshipmanager/MainActivity.java
+++ app/src/main/java/com/example/bowlingchampionshipmanager/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bowlingchampionshipmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_st  = (Button) findViewById(R.id.button_start);
+        button_st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreat1Activity();
+            }
+        });
+    }
+
+    public void openCreat1Activity(){
+        Intent intent = new Intent(this, Create1Activity.class);
+        startActivity(intent);
     }
 }
