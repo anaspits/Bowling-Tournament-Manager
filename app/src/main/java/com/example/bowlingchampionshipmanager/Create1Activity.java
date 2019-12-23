6a35ae5bf437d8cb6ebdef7e12f901c91d960163
anaspits <minianast@yahoo.gr>
2019-12-23 22:47:31 +0200

    test for improt

--- app/src/main/java/com/example/bowlingchampionshipmanager/Create1Activity.java
+++ app/src/main/java/com/example/bowlingchampionshipmanager/Create1Activity.java
@@ -3,20 +3,137 @@ package com.example.bowlingchampionshipmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.app.Activity;
 
 import static com.example.bowlingchampionshipmanager.R.id.next_btn;
+import android.widget.EditText;
 
 public class Create1Activity extends AppCompatActivity {
+
+    private static EditText textView;
+    private static final int CREATE_REQUEST_CODE = 40;
+    private static final int OPEN_REQUEST_CODE=41;
+    private static final int SAVE_REQUEST_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create1);
 
+        textView = (EditText) findViewById(R.id.fileText);
+       Button button_imp  = (Button) findViewById(R.id.button_import);
+        button_imp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFile();
+            }
+        });
 
+    }
+
+
+
+    public void openFile(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        startActivityForResult(intent, OPEN_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode,
+                                    Intent resultData) {
+
+        super.onActivityResult(requestCode, resultCode, resultData);
+        Uri currentUri = null;
+        //Cursor returnCursor = getContentResolver().query(currentUri,null,null,null,null);
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == CREATE_REQUEST_CODE) {
+                if (resultData != null) {
+                    textView.setText("");
+                }
+            } else if (requestCode == SAVE_REQUEST_CODE) {
+
+                if (resultData != null) {
+                    currentUri = resultData.getData();
+                    writeFileContent(currentUri);
+                }
+            } else if (requestCode == OPEN_REQUEST_CODE) {
+
+                if (resultData != null) {
+                    currentUri = resultData.getData();
+                   // Cursor returnCursor = getContentResolver().query(currentUri,null,null,null,null);
+                   // int fileTitle= returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+//                    String a = returnCursor.getString(fileTitle);
+                   // System.out.println(a);
+
+                    try {
+                        String content =
+                                readFileContent(currentUri);
+                        textView.setText(content);
+                       // textView.setText(returnCursor.getString(fileTitle));
+
+                    } catch (IOException e) {
+                        // Handle error here
+                    }
+                }
+            }
+        }
+    }
+
+    private void writeFileContent(Uri uri)
+    {
+        try{
+            ParcelFileDescriptor pfd =
+                    this.getContentResolver().
+                            openFileDescriptor(uri, "w");
+
+            FileOutputStream fileOutputStream =
+                    new FileOutputStream(
+                            pfd.getFileDescriptor());
+
+            String textContent =
+                    textView.getText().toString();
+
+            fileOutputStream.write(textContent.getBytes());
+
+            fileOutputStream.close();
+            pfd.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFileContent(Uri uri) throws IOException {
+
+        InputStream inputStream =
+                getContentResolver().openInputStream(uri);
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(
+                        inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String currentline;
+        while ((currentline = reader.readLine()) != null) {
+            stringBuilder.append(currentline + "\n");
+        }
+        inputStream.close();
+        return stringBuilder.toString();
     }
 
     public void openNewActivity(View View) {
---
+++ app/src/main/java/com/example/bowlingchampionshipmanager/Participant.java
@@ -0,0 +1,180 @@
+package com.example.bowlingchampionshipmanager;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Participant {
+    //Input attributes
+    int bowlerID;
+    String firstName;
+    String lastName;
+    int bowlAvg;
+    ArrayList<Participant> friends;
+
+    //Information to be decided
+    Participant partner;
+    //int laneNum; //stretch goal
+
+    //getter methods
+    public int getBowlAvg() {
+        return bowlAvg;
+    }
+
+    public String getFN(){
+        return firstName;
+    }
+
+    public String getLN(){
+        return lastName;
+    }
+
+    public Participant getPartner(){
+        return partner;
+    }
+
+    //setter methods
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public void setBowlAvg(int bowlAvg) {
+        this.bowlAvg = bowlAvg;
+    }
+
+    public void setPartner(Participant partner){
+        this.partner = partner;
+    }
+
+    //constructor
+    public Participant(int bowlerID, String firstname, String lastname, int bowlAvg) {
+        this.bowlerID = bowlerID;
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.bowlAvg = bowlAvg;
+    }
+
+    /*Comparator for sorting the list by bowlAvg*/
+    public static Comparator<Participant> partBowlAvg = new Comparator<Participant>() {
+
+        public int compare(Participant s1, Participant s2) {
+
+            int ba1 = s1.getBowlAvg();
+            int ba2 = s2.getBowlAvg();
+
+            /*For ascending order*/
+            return ba1 - ba2;
+
+            /*For descending order*/
+            //rollno2-rollno1;
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "[ Name: " + firstName + " " + lastName + ", Average: " + bowlAvg + ", Partner: " + partner.getFN() + " " + partner.getLN() + ", Partner Avg: " + partner.getBowlAvg() + "]";
+    }
+
+    /**
+     * This method fills an ArrayList of Participants with information input by a csv file.
+     *
+     *         //For each bowler imported from spreadsheet
+     *         //Create a Participant Object with listed characteristics
+     *         //Load Participant Object into list
+     *
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public ArrayList<Participant> createParticipantList(ArrayList<Participant> bowlers, String csvFile, String line, String cvsSplitBy) {
+
+        try(BufferedReader br = new BufferedReader(new FileReader(csvFile))){
+
+            String [] input = null;
+            String fn = null;
+            String ln = null;
+            int ba = 0;
+
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                // use comma as seperator
+
+                //get input
+                input = line.split(cvsSplitBy);
+//                System.out.println(Arrays.toString(input));
+
+                //get first name
+                fn = input[0];
+
+                //get last name
+                ln = input[1];
+
+                //get avg
+                ba = Integer.parseInt(input[2]);
+
+//                System.out.println("id: " + i + ", FN: " +  fn + ", LN: " + ln + ", Avg: " + ba);
+                Participant p = new Participant(i, fn, ln, ba);
+                bowlers.add(p);
+
+                i++;
+
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return bowlers;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static void main(String [] args){
+        //List to store all Participants
+        ArrayList<Participant> bowlers = new ArrayList<Participant>();
+
+        Participant s = new Participant(999,"instance", "instance", 999);
+
+        String csvFile="bowlers-list.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        s.createParticipantList(bowlers, csvFile, line, cvsSplitBy);
+
+        //Logic for generating teams(pairs)
+
+        //Sort by bowling average
+        Collections.sort(bowlers, Participant.partBowlAvg);
+        //add 5 points to pair in 2D table of poissible matchings?
+        //Associate Participants with friends
+        //2D table of friendships -- maybe remove from attributes
+        //if [][] = 2, add 2 points to pair in 2D table of poissible matchings?
+        int i;
+        for(i = 0; i < bowlers.size(); i++){
+            (bowlers.get(i)).setPartner(bowlers.get(bowlers.size() - i - 1));
+            (bowlers.get(bowlers.size() - i - 1)).setPartner(bowlers.get(i));
+
+
+        }
+
+        for(i = 0; i < bowlers.size()/2;i++){
+
+            //Print results in form of
+            //Team No. , Participant 1 , Participant 2
+
+            Participant p = bowlers.get(i);
+            System.out.println("Team " + (i + 1) + ": " + p.getFN() + " " + p.getLN() + " (Avg: " + p.getBowlAvg() + " ) & " + p.getPartner().getFN() + " " + p.getPartner().getLN() + " (Avg: " + p.getPartner().getBowlAvg() + " )");
+        }
+
+
+
+    }
+
+}
